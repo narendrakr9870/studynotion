f79219f532c9a4a6ba9462ee9a2b96a479d6ac76
narendrakr9870 <narendrakr9870@gmail.com>
2023-09-07 11:45:07 +0530

    first upload

---
+++ server/routes/profile.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+const { auth, isInstructor } = require("../middleware/auth")
+const {
+  deleteAccount,
+  updateProfile,
+  getAllUserDetails,
+  updateDisplayPicture,
+  getEnrolledCourses,
+  instructorDashboard
+} = require("../controllers/profile")
+
+// const {resetPasswordToken, resetPassword} = require("../controllers/resetPassword")
+
+// ********************************************************************************************************
+//                                      Profile routes
+// ********************************************************************************************************
+// Delet User Account
+router.delete("/deleteProfile", auth, deleteAccount)
+router.put("/updateProfile", auth, updateProfile)
+router.get("/getUserDetails", auth, getAllUserDetails)
+// Get Enrolled Courses
+router.get("/getEnrolledCourses", auth, getEnrolledCourses)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
+// router.post("/resetPasswordToken", resetPasswordToken)
+// router.post("/resetPassword", resetPassword)
+
+module.exports = router
